5ba2d236d9b7456118cd9feaf906c219daf59755
Sandro Moszczynski <sandro.moszczynski.19@ucl.ac.uk>
2019-10-17 11:52:52 +0100

    what we can do

--- group.py
+++ group.py
@@ -1,5 +1,42 @@
 """An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
+my_group=
+    my_group = {
+                    'name':'',
+                        'age':'',
+                        'job':'' }
+                   
+    Jill = {'name':'Jill',
+                'age':'26',
+                'job': 'Biologist'}
+    Zalika = {'name':'Zalika',
+                'age':'28',
+                'job': 'Artist'}
+    John = {'name':'John',
+                'age':'27',
+                'job': 'writer'}
+    Nash = {'name':'Nash',
+                'age':'34',
+                'job': 'Chef'}
+        
 
-my_group =
+
+
+
+
+
+"""
+Create an example instance, in an editor or a notebook, of a structure for this group:
+
+Jill is 26, a biologist and she is Zalika's friend and John's partner.
+Zalika is 28, an artist, and Jill's friend
+John is 27, a writer, and Jill's partner.
+Nash is 34, a chef, John's cousin and Zalika's landlord.
+Some things you may wish to consider in your model:
+
+Does it allow people who have no job?
+Does it allow people with no connections?
+Does it assume that connections are always reciprocal (e.g. if A is B's friend, does B automatically consider A a friend too?)
+
+"""
